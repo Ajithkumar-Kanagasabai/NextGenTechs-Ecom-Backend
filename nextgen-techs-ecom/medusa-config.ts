2c83e1e7f9f91468b5cfd811017c970f8f8c32a2
benitto-j24 <benittojesu@grethena.com>
2025-05-07 19:59:29 +0530

    s3 error fixed

--- nextgen-techs-ecom/medusa-config.ts
+++ nextgen-techs-ecom/medusa-config.ts
@@ -24,11 +24,10 @@ module.exports = defineConfig({
             resolve: "./src/modules/my-s3-file",
             id: "my-s3",
             options: {
-              access_key_id: process.env.AWS_ACCESS_KEY,
-              secret_access_key: process.env.AWS_SECRET_ACCESS,
-              region: process.env.AWS_S3_REGION,
-              bucket: process.env.AWS_S3_BUCKET_NAME,
-              endpoint: process.env.AWS_S3_ENDPOINT || undefined, // optional
+              access_key_id: process.env.AWS_ACCESS_KEY!,
+              secret_access_key: process.env.AWS_SECRET_ACCESS!,
+              region: process.env.AWS_S3_REGION!,
+              bucket: process.env.AWS_S3_BUCKET_NAME!,
             },
           },
         ],
--- nextgen-techs-ecom/src/services/s3-service.ts
+++ nextgen-techs-ecom/src/services/s3-service.ts
@@ -6,8 +6,6 @@ const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY!,
   secretAccessKey: process.env.AWS_SECRET_ACCESS!,
   region: process.env.AWS_S3_REGION!,
-  endpoint: `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_S3_REGION}.amazonaws.com`, // Explicit bucket endpoint
-  s3ForcePathStyle: true, // Ensures the bucket is part of the URL path instead of the domain
 });
 
 export const deleteS3Objects = async (fileKeys: string[]) => {
